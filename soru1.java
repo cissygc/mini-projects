e2347bbd2411522b2855639147d0928f3ac73de0
cissygc <cerenur06@hotmail.com>
2024-10-09 23:40:40 +0300

    my project task

--- soru1.java
+++ soru1.java
@@ -8,120 +8,129 @@ public class soru1 {
         int month = date.nextInt();
         int year = date.nextInt();
 
-        if (month>12){
+        if (month > 12) {
             System.out.println("you have entered unvalid date");
-        }else if (month<1){
+        } else if (month < 1) {
             System.out.println("you have entered unvalid date");
-        }else{
-            if (month==1){
-                if(day>31){
+        } else {
+            if (month == 1) {
+                if (day > 31) {
                     System.out.println("you have entered unvalid date");
-                }else if (1>day){
+                } else if (1 > day) {
                     System.out.println("you have entered unvalid date");
-                }else{
+                } else {
                     System.out.println("you have entered a valid date");
                 }
-            }else if (month==3){
-                if(day>31){
+            } else if (month == 3) {
+                if (day > 31) {
                     System.out.println("you have entered unvalid date");
-                }else if (1>day){
+                } else if (1 > day) {
                     System.out.println("you have entered unvalid date");
-                }else{
+                } else {
                     System.out.println("you have entered a valid date");
                 }
-            }else if (month==4){
-                if(day>30){
+            } else if (month == 4) {
+                if (day > 30) {
                     System.out.println("you have entered unvalid date");
-                }else if (1>day){
+                } else if (1 > day) {
                     System.out.println("you have entered unvalid date");
-                }else{
+                } else {
                     System.out.println("you have entered a valid date");
                 }
-            }else if (month==5){
-                if(day>31){
+            } else if (month == 5) {
+                if (day > 31) {
                     System.out.println("you have entered unvalid date");
-                }else if (1>day){
+                } else if (1 > day) {
                     System.out.println("you have entered unvalid date");
-                }else{
+                } else {
                     System.out.println("you have entered a valid date");
                 }
-            }else if (month==6){
-                if(day>30){
+            } else if (month == 6) {
+                if (day > 30) {
                     System.out.println("you have entered unvalid date");
-                }else if (1>day){
+                } else if (1 > day) {
                     System.out.println("you have entered unvalid date");
-                }else{
+                } else {
                     System.out.println("you have entered a valid date");
                 }
-            }else if (month==7){
-                if(day>31){
+            } else if (month == 7) {
+                if (day > 31) {
                     System.out.println("you have entered unvalid date");
-                }else if (1>day){
+                } else if (1 > day) {
                     System.out.println("you have entered unvalid date");
-                }else{
+                } else {
                     System.out.println("you have entered a valid date");
                 }
-            }else if(month==8){
-                if(day>31){
+            } else if (month == 8) {
+                if (day > 31) {
                     System.out.println("you have entered unvalid date");
-                }else if (1>day){
+                } else if (1 > day) {
                     System.out.println("you have entered unvalid date");
-                }else{
+                } else {
                     System.out.println("you have entered a valid date");
                 }
-            }else if (month==9){
-                if(day>30){
+            } else if (month == 9) {
+                if (day > 30) {
                     System.out.println("you have entered unvalid date");
-                }else if (1>day){
+                } else if (1 > day) {
                     System.out.println("you have entered unvalid date");
-                }else{
+                } else {
                     System.out.println("you have entered a valid date");
                 }
-            }else if (month==10){
-                if(day>31){
+            } else if (month == 10) {
+                if (day > 31) {
                     System.out.println("you have entered unvalid date");
-                }else if (1>day){
+                } else if (1 > day) {
                     System.out.println("you have entered unvalid date");
-                }else{
+                } else {
                     System.out.println("you have entered a valid date");
                 }
-            }else if (month==11){
-                if(day>30){
+            } else if (month == 11) {
+                if (day > 30) {
                     System.out.println("you have entered unvalid date");
-                }else if (1>day){
+                } else if (1 > day) {
                     System.out.println("you have entered unvalid date");
-                }else{
+                } else {
                     System.out.println("you have entered a valid date");
                 }
-            }else if (month==12){
-                if(day>31){
+            } else if (month == 12) {
+                if (day > 31) {
                     System.out.println("you have entered unvalid date");
-                }else if (1>day){
+                } else if (1 > day) {
                     System.out.println("you have entered unvalid date");
-                }else{
+                } else {
                     System.out.println("you have entered a valid date");
                 }
-            }else{
+            } else {
+
                 boolean year1 = false;
-                if((year%4==0)&&(year%100==0)){
-                    year1=false;
-                }else if(year%400==0){
-                    year1=true;
+                if (year % 4 == 0) {
+                    if (year % 100 == 0) {
+                        if (year % 400 == 0) {
+                            year1 = true;
+                        } else {
+                            year1 = false;
+                        }
+                    } else {
+                        year1 = true;
+                    }
                 }
-                if(year1){
-                    if(day>29){
+
+
+                if (year1) {
+                    if (day > 29) {
                         System.out.println("you have entered unvalid date");
-                    }else if (1>day){
+                    } else if (1 > day) {
                         System.out.println("you have entered unvalid date");
-                    }else{
+                    } else {
                         System.out.println("you have entered a valid date");
                     }
-                }else{
-                    if(day>28){
+                } else {
+                    if (day > 28) {
                         System.out.println("you have entered unvalid date");
-                    }else if (1>day){
+                    } else if (1 > day) {
                         System.out.println("you have entered unvalid date");
-                    }else{
+                    } else {
                         System.out.println("you have entered a valid date");
                     }
                 }
